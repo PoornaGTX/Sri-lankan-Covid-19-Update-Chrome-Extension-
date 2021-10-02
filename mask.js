39727f632e2e98046ac41f51af7e606ce6f0f71c
PoornaGTX <onlinepoorna@gmail.com>
2021-10-02 11:13:48 +0530

    70% done

--- mask.js
+++ mask.js
@@ -1,35 +1,30 @@
+ 
+ function getData(){
+             fetch('https://www.hpb.health.gov.lk/api/get-current-statistical')
+             .then((apidata)=> {
+                    console.log(apidata);
+                    return apidata.json();
+
+              }).then((actualdata) => {
+
+                    const mydata = actualdata.data;
+
+                        
+                    
+                  document.getElementById('newcases').textContent = actualdata.data.local_new_cases;
+                  document.getElementById('newtotal').textContent = actualdata.data.local_total_cases;
+                  document.getElementById('newrecoverd').textContent = actualdata.data.local_recovered;
+                  document.getElementById('newupdate').textContent = actualdata.data.update_date_time;
+                  
+
+                    console.log(actualdata);
+
+                }).catch((error) => {
+
+                });
 
 
+            
+            }
 
-    fetch('https://www.hpb.health.gov.lk/api/get-current-statistical').then( (apidata)
-    
-    => {
-            console.log(apidata);
-            return apidata.json();
-
-
-
-    }).then((actualdata)=>{
-
-
-    }).catch((error)=>{
-
-    });
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-    document.getElementById('newcases').textContent = local_new_cases;
-    document.getElementById('newtotal').textContent = local_total_cases;
-    document.getElementById('newrecoverd').textContent = local_new_cases;
+getData();
